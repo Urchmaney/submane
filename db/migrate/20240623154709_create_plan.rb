c0ee0fb58896f7750b0d3a9ce4c82ab6103586e2
kingsley-people-box <kingsley@peoplebox.ai>
2024-06-26 14:46:55 +0100

    add back price to plan

--- db/migrate/20240623154709_create_plan.rb
+++ db/migrate/20240623154709_create_plan.rb
@@ -2,7 +2,7 @@ class CreatePlan < ActiveRecord::Migration[7.1]
   def change
     create_table :plans do |t|
       t.string :name
-      # t.integer :price
+      t.integer :price
       t.integer :visual_order
 
       t.timestamps
