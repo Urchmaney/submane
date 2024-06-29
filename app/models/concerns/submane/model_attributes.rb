f822bb04bdc248eb175e5420e32acf6452f0a5d0
kingsley-people-box <kingsley@peoplebox.ai>
2024-06-29 19:38:08 +0100

    fix lint error

--- app/models/concerns/submane/model_attributes.rb
+++ app/models/concerns/submane/model_attributes.rb
@@ -6,19 +6,19 @@ module Submane
       def create_subscription; end
 
       def active_subcriptions
-        subscriptions.where('end_date > ?', Time.now).order(:end_date)
+        subscriptions.where("end_date > ?", Time.now).order(:end_date)
       end
 
       def active_plan
-        subscriptions.where('end_date > ?', Time.now).order(:end_date).last.submane_plan
+        subscriptions.where("end_date > ?", Time.now).order(:end_date).last.submane_plan
       end
 
       def active_plan?
-        subscriptions.where('end_date > ?', Time.now).exists?
+        subscriptions.where("end_date > ?", Time.now).exists?
       end
 
       def active_subscription
-        subscriptions.where('end_date > ?', Time.now).order(:end_date).last
+        subscriptions.where("end_date > ?", Time.now).order(:end_date).last
       end
 
       alias_method :subcribed?, :active_plan?
