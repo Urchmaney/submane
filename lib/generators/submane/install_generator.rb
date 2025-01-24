c30b9a0b82da8de9a49b8bc8820153ead8772767
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-24 15:54:56 +0100

    move migration files to be generated after installation

---
+++ lib/generators/submane/install_generator.rb
@@ -0,0 +1,24 @@
+require "rails/generators/active_record"
+
+module Submane
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include ActiveRecord::Generators::Migration
+      source_root File.expand_path("templates", __dir__)
+
+      def copy_migration
+        migration_template "migration.rb", "db/migrate/install_submane.rb", migration_version: migration_version
+      end
+
+      def copy_initializer_config
+        copy_file "config.rb", "config/initializers/submane.rb"
+      end
+
+      private
+
+      def migration_version
+        "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
+      end
+    end
+  end
+end
