d22199be40d8de02ce8bc578b10472d5000e6c47
Unegbu Kingsley <kingsobino@gmail.com>
2025-02-22 12:48:32 +0100

    fix failing test and rubocop

---
+++ test/lib/generators/tmp/db/migrate/20250222114741_install_submane.rb
@@ -0,0 +1,19 @@
+class InstallSubmane < ActiveRecord::Migration[7.1]
+  def change # rubocop:disable Metrics/MethodLength
+    create_table :submane_plans do |t|
+      t.string :name
+      t.integer :price
+      t.integer :visual_order, index: { unique: true }
+
+      t.timestamps
+    end
+
+    create_table :submane_subscriptions do |t|
+      t.references :plan
+      t.references :subscriber
+      t.datetime :end_date, null: false
+
+      t.timestamps
+    end
+  end
+end
