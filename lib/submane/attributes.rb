a3cd26d6cb893ed22e31cb2255e96a2644556d6d
Unegbu Kingsley <kingsobino@gmail.com>
2025-02-24 17:28:48 +0100

    rearrange attributes code

--- lib/submane/attributes.rb
+++ lib/submane/attributes.rb
@@ -1,4 +1,33 @@
 module Submane
+  module Model
+    def setup_submane
+      has_many :subscriptions, class_name: "Submane::Subscription", foreign_key: "subscriber_id"
+      include Attributes
+    end
+
+    module Attributes
+      def create_subscription; end
+
+      def active_subcriptions
+        subscriptions.where("end_date > ?", Time.now).order(:end_date)
+      end
+
+      def active_plan
+        subscriptions.where("end_date > ?", Time.now).order(:end_date).last.submane_plan
+      end
+
+      def active_plan?
+        subscriptions.where("end_date > ?", Time.now).exists?
+      end
+
+      def active_subscription
+        subscriptions.where("end_date > ?", Time.now).order(:end_date).last
+      end
+
+      alias subcribed? active_plan?
+    end
+  end
+
   module Attributes
     def plan_active_subscriptions(plan_id)
       Submane::Subscription.where("plan_id = ? AND end_date > ?", plan_id, Time.now)
--- test/models/submane/plan_test.rb
+++ test/models/submane/plan_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 module Submane
   class PlanTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "Plan counts is correct" do
+      assert_equal Submane::Plan.count, 2
+    end
   end
 end
